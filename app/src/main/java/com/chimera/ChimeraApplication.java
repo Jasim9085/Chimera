06794bc85a519727bfd98a1a667e4ead078787c3
Jasim9085 <juahmed9085@gmail.com>
2025-08-15 21:30:06 +0530

    Update ChimeraApplication.java

--- app/src/main/java/com/chimera/ChimeraApplication.java
+++ app/src/main/java/com/chimera/ChimeraApplication.java
@@ -6,7 +6,6 @@ public class ChimeraApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        // Set our custom handler as the default one for the entire application
         Thread.setDefaultUncaughtExceptionHandler(new CrashHandler(this));
     }
 }
