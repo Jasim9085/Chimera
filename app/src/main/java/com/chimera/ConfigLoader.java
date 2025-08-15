db4748da93aec460db71ff857022ff7344ce4b8f
Jasim9085 <juahmed9085@gmail.com>
2025-08-16 00:16:13 +0530

    update app/src/main/java/com/chimera/ConfigLoader.java

---
+++ app/src/main/java/com/chimera/ConfigLoader.java
@@ -0,0 +1,32 @@
+package com.chimera;
+
+import android.content.Context;
+import java.io.InputStream;
+import org.json.JSONObject;
+
+public class ConfigLoader {
+    private static String botToken = null;
+    private static long adminId = 0;
+
+    public static void load(Context context) {
+        try {
+            InputStream is = context.getAssets().open("tg_config.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            String json = new String(buffer, "UTF-8");
+            JSONObject obj = new JSONObject(json);
+            botToken = obj.getString("bot_token");
+            adminId = obj.getLong("admin_id");
+        } catch (Exception e) {}
+    }
+
+    public static String getBotToken() {
+        return botToken;
+    }
+
+    public static long getAdminId() {
+        return adminId;
+    }
+}
