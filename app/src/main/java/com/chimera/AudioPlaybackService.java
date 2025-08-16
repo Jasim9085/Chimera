48157db8a2a63ed7b0b894f8638f0bef22dd5890
Jasim9085 <juahmed9085@gmail.com>
2025-08-16 15:57:57 +0530

    update app/src/main/java/com/chimera/AudioPlaybackService.java

--- app/src/main/java/com/chimera/AudioPlaybackService.java
+++ app/src/main/java/com/chimera/AudioPlaybackService.java
@@ -4,53 +4,59 @@ import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Handler;
 import android.os.IBinder;
 import java.io.File;
 
 public class AudioPlaybackService extends Service {
 
     private MediaPlayer mediaPlayer;
+    private Handler stopHandler = new Handler();
+    private Runnable stopRunnable;
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
+    @Override public IBinder onBind(Intent intent) { return null; }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         if (intent != null && "ACTION_PLAY_AUDIO".equals(intent.getAction())) {
             String audioPath = intent.getStringExtra("audioPath");
-            playAudio(audioPath);
+            int duration = intent.getIntExtra("duration", -1);
+            playAudio(audioPath, duration);
         }
         return START_NOT_STICKY;
     }
 
-    private void playAudio(String audioPath) {
-        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
-            mediaPlayer.stop();
-            mediaPlayer.release();
-        }
+    private void playAudio(String audioPath, int duration) {
+        stopPlayback();
         mediaPlayer = new MediaPlayer();
         try {
             mediaPlayer.setDataSource(this, Uri.fromFile(new File(audioPath)));
             mediaPlayer.prepare();
             mediaPlayer.start();
             mediaPlayer.setOnCompletionListener(mp -> stopSelf());
+
+            if (duration > 0) {
+                stopRunnable = () -> stopSelf();
+                stopHandler.postDelayed(stopRunnable, duration * 1000L);
+            }
         } catch (Exception e) {
             ErrorLogger.logError(this, "AudioPlayback", e);
             stopSelf();
         }
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
+    private void stopPlayback() {
+        if (stopRunnable != null) stopHandler.removeCallbacks(stopRunnable);
         if (mediaPlayer != null) {
-            if (mediaPlayer.isPlaying()) {
-                mediaPlayer.stop();
-            }
+            if (mediaPlayer.isPlaying()) mediaPlayer.stop();
             mediaPlayer.release();
             mediaPlayer = null;
         }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopPlayback();
     }
 }
